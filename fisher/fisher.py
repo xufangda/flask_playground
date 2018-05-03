bb787d62f4efcdc7e75803c7de3f5af6687e3ae2
fangda <xfangda@gmail.com>
2018-05-03 21:15:57 +0100

    301 status tutorial

--- fisher/fisher.py
+++ fisher/fisher.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -7,11 +7,17 @@ app.config.from_object('config')
 
 @app.route('/hello')
 def hello():
-    return 'hello, jiuyue'
+    headers ={
+        'content-type':'text/plain',
+        'location':'http://www.bing.com'
+    }
+    response = make_response('<html></html>',301)
+    response.headers=headers
+    return response
 
 # 当使用基于类的视图（即插视图）时，用下边这个函数注册
 # app.add_url_rule('/hello',view_func=hello)
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.debug=app.config['DEBUG']
     app.run(host='0.0.0.0')
