650a247114e31426f50511b9a0b4bc04d1f19dbb
fangda <xfangda@gmail.com>
2018-05-11 00:15:52 +0100

    OOP reconstruction

--- fisher/app/view_models/book.py
+++ fisher/app/view_models/book.py
@@ -1,4 +1,26 @@
+
 class BookViewModel:
+    def __init__(self, book):
+        self.title = book['title']
+        self.publisher =book['publisher']
+        self.author='、'.join(book['author'])
+        self.image=book['image']
+        self.price = book['price']
+        self.summary = book['summary']
+        self.pages =book['pages']
+
+class BookCollection:
+    def __init__(self):
+        self.total=0
+        self.books=[]
+        self.keyword=''
+
+    def fill (self, yushu_book, keyword):
+        self.total=yushu_book.total
+        self.keyword = keyword
+        self.books=[BookViewModel(book) for book in yushu_book.books]
+
+class _BookViewModel:
     @classmethod
     def package_single(cls, data, keyword):
         returned = {
--- fisher/app/web/book.py
+++ fisher/app/web/book.py
@@ -4,9 +4,10 @@ from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
 from app.forms.book import SearchForm
 
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookCollection
 
 from . import web
+import json
 
 @web.route('/book/search')
 def search(m=1):
@@ -23,17 +24,19 @@ def search(m=1):
 
     #验证层
     form =SearchForm(request.args)
+    books = BookCollection()
     if form.validate():
         q=form.q.data.strip()
         page=form.page.data
         isbn_or_key=is_isbn_or_key(q)
+        yushu_book=YuShuBook()
         
         if isbn_or_key=='isbn':
-            result = YuShuBook.search_by_isbn(q)
-            result = BookViewModel.package_single(result,q)
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q,page)
-            result = BookViewModel.package_collection(result,q)
-        return jsonify(result)
+            yushu_book.search_by_keyword(q,page)
+
+        books.fill(yushu_book, q)
+        return json.dumps(books,default = lambda o:o.__dict__),
     else:
         return jsonify(form.errors)
